3a5b311287c9bae907064b068b648d6c51288fc2
lxf <rdxer@foxmail.com>
2020-01-03 02:31:27 +0800

    完善 登陆接口

--- src/main/java/com/rdxer/jwtsso/security/JWTLoginFilter.java
+++ src/main/java/com/rdxer/jwtsso/security/JWTLoginFilter.java
@@ -5,12 +5,14 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Data;
 import lombok.SneakyThrows;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -18,7 +20,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JWTLoginFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -26,6 +31,7 @@ public class JWTLoginFilter extends UsernamePasswordAuthenticationFilter {
 
     public JWTLoginFilter(AuthenticationManager authenticationManager) {
         this.setAuthenticationManager(authenticationManager);
+        this.setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
     }
 
     /**
@@ -99,4 +105,30 @@ public class JWTLoginFilter extends UsernamePasswordAuthenticationFilter {
 class LoginPara{
     private String username;
     private String password;
+}
+
+class JWTAuthenticationFailureHandler
+        implements AuthenticationFailureHandler {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void onAuthenticationFailure(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException exception)
+            throws IOException, ServletException {
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        Map<String, Object> data = new HashMap<>();
+        data.put(
+                "timestamp",
+                Calendar.getInstance().getTime());
+        data.put(
+                "exception",
+                exception.getMessage());
+
+        response.getOutputStream()
+                .println(objectMapper.writeValueAsString(data));
+    }
 }
